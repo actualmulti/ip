6bad16436c58e00ac44ef0de215c3d9072f4d784
mitchel <mitchel0303@gmail.com>
2025-08-23 14:49:55 +0800

    feat: renamed chatbot to JAKE

---
+++ src/main/java/Jake.java
@@ -0,0 +1,16 @@
+public class Jake {
+    public static void main(String[] args) {
+        String logo =
+                "     _   _    _  _______ \n"
+                        + "    | | / \\  | |/ / ____|\n"
+                        + " _  | |/ _ \\ | ' /|  _|  \n"
+                        + "| |_| / ___ \\| . \\| |___ \n"
+                        + " \\___/_/   \\_\\_|\\_\\_____|\n";
+
+        System.out.println("Hello from\n" + logo);
+        System.out.println("What can I do for you today?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________\n");
+    }
+}
