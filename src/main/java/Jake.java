f30fd762332f1cd5e77a4dcd2abeaacd9a3ee9c5
mitchel <mitchel0303@gmail.com>
2025-08-23 15:02:22 +0800

    feat: echo feature

--- src/main/java/Jake.java
+++ src/main/java/Jake.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Jake {
     public static void main(String[] args) {
+        // Initialize the scanner
+        Scanner scanner = new Scanner(System.in);
+
+        // JAKE logo
         String logo =
                 "     _   _    _  _______ \n"
                         + "    | | / \\  | |/ / ____|\n"
@@ -10,7 +16,23 @@ public class Jake {
         System.out.println("Hello from\n" + logo);
         System.out.println("What can I do for you today?");
         System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________\n");
+
+        // Chat logic
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+
+            System.out.println("____________________________________________________________");
+            System.out.println(input);
+            System.out.println("____________________________________________________________");
+        }
+
+
     }
 }
