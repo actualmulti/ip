e728794b1191bc6e6ed84e2f24244bacdf9866a1
mitchel <mitchel0303@gmail.com>
2025-09-13 20:33:14 +0800

    Crop profile pictures to become circular

--- src/main/java/DialogBox.java
+++ src/main/java/DialogBox.java
@@ -1,6 +1,8 @@
 import java.io.IOException;
 import java.util.Collections;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -8,9 +10,12 @@ import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
+import javafx.scene.effect.DropShadow;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 
 /**
  * Represents a dialog box consisting of an ImageView to represent the speaker's face
@@ -34,6 +39,8 @@ public class DialogBox extends HBox {
 
         dialog.setText(text);
         displayPicture.setImage(img);
+
+        applyCircularClip();
     }
 
     /**
@@ -54,5 +61,28 @@ public class DialogBox extends HBox {
         var db = new DialogBox(text, img);
         db.flip();
         return db;
+    }
+
+    // ChatGPT-5 was consulted to come up with such a feature
+    private void applyCircularClip() {
+        // Use the fit sizes you set in FXML (99x99) rather than actual image width (safer if image is large)
+        double diameter = Math.min(displayPicture.getFitWidth(), displayPicture.getFitHeight());
+        if (diameter <= 0) {
+            // Defer until layout pass if sizes not resolved yet
+            displayPicture.layoutBoundsProperty().addListener(new ChangeListener<>() {
+                @Override
+                public void changed(ObservableValue<? extends javafx.geometry.Bounds> obs,
+                                    javafx.geometry.Bounds oldB, javafx.geometry.Bounds newB) {
+                    displayPicture.layoutBoundsProperty().removeListener(this);
+                    applyCircularClip(); // retry
+                }
+            });
+            return;
+        }
+        double radius = diameter / 2.0;
+        Circle clip = new Circle(radius, radius, radius);
+        displayPicture.setClip(clip);
+
+        displayPicture.setEffect(new DropShadow(10, Color.rgb(0, 0, 0, 0.25)));
     }
 }
